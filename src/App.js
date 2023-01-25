574ff5cea8bc61bec4f211ba7bb0e47c77159fd1
vsheva <valera.sheva@gmail.com>
2023-01-24 22:27:28 -0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import './App.css';
-import  { useEffect, useState } from 'react';
-import axios from "axios"
-import MovieCard from "./component/MovieCard";
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieCard from './component/MovieCard';
 
 function App() {
+  const [movies, setMovies] = useState([]); //*
 
-    const[movies, setMovies]= useState([]) //*
-
-
-  const getMovies=async()=>{
-    try{
-    const data = await axios.get('https://movies-app.prakashsakari.repl.co/api/movies').then((data)=>{
-       setMovies(data.data)
-     })
+  const getMovies = async () => {
+    try {
+      const data = await axios
+        .get('https://movies-app.prakashsakari.repl.co/api/movies')
+        .then(data => {
+          setMovies(data.data);
+        });
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-        console.log(err)
-    }
-  }
+  };
 
-  useEffect(()=>{
-      getMovies()
-  }, [])
-
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
-      <div className="App">
-          <header className='header'>
-              <h1 className="header-movie">Movies</h1>
-          </header>
+    <div className="App">
+      <header className="header">
+        <h1 className="header-movie">Movies</h1>
+      </header>
 
-          <main className="main">
-              { movies && movies.length>0 && movies.map(movie=> <MovieCard  key={movie.id} movi={movie}/>)}
-
-          </main>
-      </div>
-
+      <main className="main">
+        {movies &&
+          movies.length > 0 &&
+          movies.map(movie => <MovieCard key={movie.id} movi={movie} />)}
+      </main>
+    </div>
   );
 }
 
